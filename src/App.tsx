4adf058397bb3d10b7e7a1466a1d8f213c4473ad
ekaputra04 <ekaputrajuniawan@gmail.com>
2025-02-22 11:35:32 +0800

    feat: add Navbar component and integrate Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <h1 className="font-bold text-3xl underline">Hello world!</h1>
+      <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,53 @@
+type NavbarItemType = {
+  title: string;
+  link: string;
+};
+
+const NavbarItems: NavbarItemType[] = [
+  {
+    title: "Home",
+    link: "#",
+  },
+  {
+    title: "What We Do",
+    link: "#what-we-do",
+  },
+  {
+    title: "Service",
+    link: "#service",
+  },
+  {
+    title: "Project",
+    link: "#project",
+  },
+  {
+    title: "Blog",
+    link: "#blog",
+  },
+  {
+    title: "Contact",
+    link: "#contact",
+  },
+];
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="flex justify-between items-center px-32 py-8">
+        <img src="/images/logo.png" alt="logo" className="w-28" />
+        <ul className="flex gap-6">
+          {NavbarItems.map((item, index) => (
+            <li key={index} className="inline-block mx-4">
+              <a
+                href={item.link}
+                className="font-semibold text-gray-800 hover:text-blue-500 text-base"
+              >
+                {item.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
